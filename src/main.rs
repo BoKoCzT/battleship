411b5ccb10d205e3ae4dbf66676eba5299160c5c
BoKo <boko@ttonline.sk>
2024-09-02 20:08:03 +0200

    Medzistav

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+// Imports of needed libraries
+use std::io::{self, write};
+use rand::rng;
+
+// We define size of the game board
+const BOARD_SIZE: usize = 10;
+
+struct Board{
+    grid: [[CellState; BOARD_SIZE]; BOARD_SIZE],
+    ships: Vec<(usize, usize)>,
+}
+
+enum CellState{
+    Empty,
+    Ship,
+    Hit,
+    Miss
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+
+    }
+}
+
+fn get_player_input() {
+
+}
+
+fn generate_opponent_move() {
+
 }
